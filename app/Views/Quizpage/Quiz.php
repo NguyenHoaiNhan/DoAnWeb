0ad4a6ea966febb013fc7a4c8be662e1cf69f5aa
NguyenHoaiNhan <nhan.nguyenhoai9648@gmail.com>
2021-11-29 13:14:33 +0700

    Set up CodeIgniter .env

---
+++ app/Views/Quizpage/Quiz.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <script type="text/javascript" src="mathjax-config.js" defer></script>
+    <script type="text/javascript" id="MathJax-script" defer src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+    </script>
+</head>
+
+<body>
+    <div>
+        \[a^2 + b^2 = c^2\]
+        <br>
+        <p>Đây là đoạn code dùng mathjax với $x = $10</p>
+    </div>
+</body>
+
+</html>
